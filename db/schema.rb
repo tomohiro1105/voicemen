7356a04287c6eae04311924b2b5b3463c2ced872
tomohiro1105 <paripari500@icloud.com>
2020-01-08 16:37:10 +0900

    ログイン機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_07_080929) do
+ActiveRecord::Schema.define(version: 2020_01_08_073357) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -18,9 +18,9 @@ ActiveRecord::Schema.define(version: 2020_01_07_080929) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -30,6 +30,9 @@ ActiveRecord::Schema.define(version: 2020_01_07_080929) do
     t.text "content_url", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_words_on_user_id"
   end
 
+  add_foreign_key "words", "users"
 end
